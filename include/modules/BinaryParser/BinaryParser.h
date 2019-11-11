2c0608bd0e35a6e856c5c1df92221a919f55898a
Bob Costas <finkzane@gmail.com>
2019-11-11 14:04:48 -0800

    added constructor, class member

--- include/modules/BinaryParser/BinaryParser.h
+++ include/modules/BinaryParser/BinaryParser.h
@@ -5,4 +5,34 @@
 #ifndef BINARY_PARSER_HH_INCLUDED
 #define BINARY_PARSER_HH_INCLUDED
 
-#endif //GENTEST_BINARYPARSER_H
+#include <bitset>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+/**
+ * Type alias byte to unsigned char 
+ * to make its use more clear.
+ **/
+using byte = unsigned char;
+
+/**
+ * A BinaryParser reads unstructured binary data 
+ * from a filestream and stores the data internally. 
+ **/
+class BinaryParser
+{
+ public:
+    BinaryParser() = default;
+
+ private:
+
+    /**
+     * Represents unstructured binary data.
+     * Each byte is represented by an unsigned char.
+     **/
+    std::vector<byte> data;
+};
+
+#endif // BINARY_PARSER_HH_INCLUDED
