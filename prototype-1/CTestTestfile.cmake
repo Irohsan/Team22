23209c6904e8ce5b1e59458d7c941ffdcd050a8d
Tristan Miller <tem83@nau.edu>
2019-12-08 16:34:08 -0700

    Restructured files to be more inline with other branches

--- prototype-1/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/iroh/Documents/CS476/GenTest/Team22/prototype-1
-# Build directory: /home/iroh/Documents/CS476/GenTest/Team22/prototype-1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test "/home/iroh/Documents/CS476/GenTest/Team22/prototype-1/gentest_test")
