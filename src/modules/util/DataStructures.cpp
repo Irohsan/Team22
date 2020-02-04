cff7e5512d02d3d2a443a2540d78f458eac490e5
Tristan Miller <tem83@nau.edu>
2020-02-03 19:45:03 -0700

    Creation of Searchable TranslationDictionary generated via config files

--- src/modules/util/DataStructures.cpp
+++ src/modules/util/DataStructures.cpp
@@ -613,4 +613,140 @@ void Line::toString( bool includeSemiColon )
     std::cout << this->getString( includeSemiColon ) << "\n";
 }
 
+//Translation Entry Methods
+
+void TranslationEntry::appendToEnd( std::string nTerminalVal, std::string translateTo )
+{
+    //if current index is end of list
+    if( this->nextEntry == nullptr )
+    {
+        this->nextEntry = new TranslationEntry;
+
+        this->nextEntry->nTerminalVal = nTerminalVal;
+
+        this->nextEntry->translateTo = translateTo;
+    } else
+    {
+        this->nextEntry->appendToEnd( nTerminalVal, translateTo );
+    }
+}
+
+bool TranslationEntry::assignTranslation(std::string translation, NonTerminals toAssign )
+{
+    if( this == nullptr )
+    {
+        //TODO: Log not finding a translation in the entries
+        return false;
+    }
+    //if this translation is correct
+    if( this->nTerminalVal == translation )
+    {
+        this->nTerminal = toAssign;
+
+        return true;
+    }
+
+    return this->nextEntry->assignTranslation(translation, toAssign);
+}
+
+TranslationEntry* TranslationEntry::findTranslationFromNTerminal( NonTerminals NTerminalToFind )
+{
+    if( this == nullptr )
+    {
+        //TODO: Log this
+
+        return nullptr;
+    }
+    if( NTerminalToFind == this->nTerminal )
+    {
+        return this;
+    }
+
+    return this->nextEntry->findTranslationFromNTerminal( NTerminalToFind );
+}
+
+//Translation Dictionary Methods
+
+void TranslationDictionary::setFile(const std::string& filePath )
+{
+    configFile.open( filePath );
+}
+
+bool TranslationDictionary::loadFile()
+{
+    //loops over each line of cfg file
+    while( !configFile.eof() )
+    {
+        std::string currentTrans, nTerminal, translateTo;
+
+        std::getline( configFile, currentTrans );
+
+        int location = currentTrans.find('=');
+
+        //if invalid translation
+        if( location == std::string::npos )
+        {
+            //log if this happens
+
+            return false;
+        }
+
+        nTerminal = currentTrans.substr(0, location );
+
+        translateTo = currentTrans.substr( location + 1 );
+
+        //if first entry
+        if( translations == nullptr )
+        {
+            translations = new TranslationEntry();
+
+            translations->nTerminalVal = nTerminal;
+
+            translations->translateTo = translateTo;
+        }
+        //else append to end
+        else {
+            translations->appendToEnd( nTerminal, translateTo );
+        }
+    }
+
+    return populateNTerminals();
+}
+
+TranslationEntry * TranslationDictionary::findTranslationFromNTerminal( NonTerminals NTerminalToFind )
+{
+    return translations->findTranslationFromNTerminal( NTerminalToFind );
+}
+
+/**
+ * Private method to populate the translation entries with their proper NTerminal
+ * @return If the population was successful.
+ */
+bool TranslationDictionary::populateNTerminals()
+{
+    auto it = vitalTranslations.begin();
+
+    while( it != vitalTranslations.end() )
+    {
+        std::string currentNTerminalVal = it->first;
+
+        NTerminal currentNTerminal = it->second;
+
+        bool populated = translations->assignTranslation( currentNTerminalVal, currentNTerminal );
+
+        //if a vitalTranslation wasn't populated
+        if( !populated )
+        {
+            //TODO: Log vital translation not being populated
+
+            return false;
+        }
+        //increment the iterator for the next vital translation
+        it++;
+
+    }
+
+    return true;
+}
+
 
